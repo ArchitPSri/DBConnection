f69f135cfd62e295e29978e154b56ffab93c8614
Archit Srivastava <apsrivastava0909@gmail.com>
2018-11-26 23:39:43 +0530

    Added DB Connection class

---
+++ DBConnection/DBConnection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace DBConnection
+{
+    public class DBConnectionClass
+    {
+        public DBConnectionClass()
+        {
+        }
+
+        public int Demo(int a)
+        {
+            return a + 10;
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnectionClass _instance = null;
+        public static DBConnectionClass Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnectionClass();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connstring = string.Format("Server=localhost; database={0}; UID=UserName; password=your password", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
